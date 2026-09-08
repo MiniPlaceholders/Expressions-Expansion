d08bc0e2f7cf13de0eb373fd83d76ac63604040c
Adrian Gonzales <adriangonzalesval@gmail.com>
2026-09-07 21:28:25 -0500

    misc: Updated Adventure
    closes #162

--- common/src/main/java/module-info.java
+++ common/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module io.github.miniplaceholders.expressions.common {
     requires io.github.miniplaceholders.api;
-    requires net.kyori.adventure;
+    requires net.kyori.adventure.api;
     requires net.kyori.adventure.text.minimessage;
     requires net.kyori.adventure.text.serializer.plain;
     requires org.jetbrains.annotations;
